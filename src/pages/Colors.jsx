506898e13fa7ecc124fae9bb9e604d6146706a28
SpiriT <x-spirit-alex@mail.ru>
2022-07-07 11:53:02 +0300

    fix nn data

--- src/pages/Colors.jsx
+++ src/pages/Colors.jsx
@@ -13,9 +13,9 @@ const Colors = () => {
     
     const colorChanged = (value) => {
         const color = {
-            r: value?.rgb.r,
-            g: value?.rgb.g,
-            b: value?.rgb.b
+            r: value?.rgb.r/255,
+            g: value?.rgb.g/255,
+            b: value?.rgb.b/255
         }
         setRgb(color)
         
@@ -25,11 +25,12 @@ const Colors = () => {
         for (let item in resultNN) {
             sortable.push({
                 color: item,
+                valueNum: Math.round(resultNN[item] * 100 ) / 100,
                 value: (resultNN[item] * 100).toFixed(2) + '%'
             })
         }
         sortable.sort(function(a, b) {
-            return a.value < b.value ? 1 : -1
+            return b.valueNum - a.valueNum
         });
 
         setArray(sortable)
@@ -37,24 +38,20 @@ const Colors = () => {
 
     const getRGB = (colorName) => {
         const color = Array.from(data).find((item) => item.output[colorName])?.input
-        return `rgb(${color.r},${color.g},${color.b})`
-    }
-    const getRGBValue = (colorName) => {
-        const color = Array.from(data).find((item) => item.output[colorName])?.input
-        return `[${color.r},${color.g},${color.b}]`
+        return `rgb(${color.r*255},${color.g*255},${color.b*255})`
     }
 
     useEffect(() => {
         const net = new NeuralNetwork()
-        const testData = data
 
-        net.train(testData)
+        net.train(data)
         
         setNet(net)
     }, [])
 
     return (
         <Space direction="vertical">
+        <Title level={4}>Выберите цвет</Title>
             <Space>
                 <Form>
                     <Form.Item name={`color`}>
@@ -64,25 +61,20 @@ const Colors = () => {
                             onChange={(value) => colorChanged(value)} />
                     </Form.Item>
                 </Form>
-                <Space direction={'vertical'}>
-                    <p>r = {rgb?.r}</p>
-                    <p>g = {rgb?.g}</p>
-                    <p>b = {rgb?.b}</p>
-                </Space>
             </Space>
             <Divider></Divider>
-            <Title level={5}> Результаты</Title>
-            {array?.map((item) => (
-                <div key={item.color} style={{display: 'flex', alignItems: 'center'}}>
-                    <div style={{
-                        height: 20,
-                        width: 50,
-                        backgroundColor: getRGB(item.color),
-                        margin: 10
-                    }}></div>
-                    {item.color} {item.value}
-                </div>
-            ))}
+            <Title level={5}>Результаты нейронной сети</Title>
+                {array?.map((item) =>
+                    <div key={item.color} style={{display: 'flex', alignItems: 'center'}}>
+                        <div style={{
+                            height: 20,
+                            width: 50,
+                            backgroundColor: getRGB(item.color),
+                            margin: 10
+                        }}></div>
+                        {item.color} {item.value}
+                    </div>
+                )}
         </Space>
     )
 }
